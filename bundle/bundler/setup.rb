8fbb2c5c9bba59ebb63465c408d88913c9be5039
Juan Camilo Bages <jcbages@outlook.com>
2020-08-16 15:26:37 -0700

    update .gitignore

--- bundle/bundler/setup.rb
+++
@@ -1,9 +0,0 @@
-require 'rbconfig'
-ruby_engine = RUBY_ENGINE
-ruby_version = RbConfig::CONFIG["ruby_version"]
-path = File.expand_path('..', __FILE__)
-$:.unshift "#{path}/"
-$:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/gems/multipart-post-2.1.1/lib"
-$:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/gems/faraday-1.0.1/lib"
-$:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/gems/faraday-1.0.1/spec/external_adapters"
-$:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/gems/thor-1.0.1/lib"
